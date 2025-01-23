60a0a7fc46d5fe091f4ce75df4d3d5f56b5e24fe
SifatSararChistee <sifatsararchistee@gamil.com>
2025-01-23 12:38:37 +0600

    implemented kinde auth

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,21 @@
-'use client'
+import {RegisterLink, LoginLink , LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from 'next/link';
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = async() => {
+        const {getUser} = getKindeServerSession();
+        const user = await getUser();
     return (
-        <div className='font-bold text-xl flex justify-center gap-5 mt-5'>
-            <Link href='/'>Home</Link>
-            <Link href='/profile'>Profile</Link>
+        <div className='font-bold text-xl flex justify-center items-center gap-5 bg-slate-200 py-4'>
+            <Link href='/' className="btn">Home</Link>
+            {user ? <>
+            <Link href='/profile' className="btn">Profile</Link>
+            <LogoutLink className="btn">Log out</LogoutLink></>
+            :
+            <><LoginLink className="btn">Log in</LoginLink>
+            <p>OR</p>
+            <RegisterLink className="btn">Sign up</RegisterLink></>}
         </div>
     );
 };
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const page = () => {
+const page = async() => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
     return (
         <div>
-            this is profile page
+            {
+                user ? 
+            <><div className='flex items-center justify-center flex-col gap-3 mt-5'>
+                <h2 className='text-3xl'>Welcome to the Profile page</h2>
+            <img className='rounded-full' src={user.picture} alt={user.family_name} />
+            <p>{user.family_name}</p>
+            <p>{user.email}</p>
+            </div>
+            </>
+            :<LoginLink className='btn'>Login</LoginLink>
+            }
         </div>
     );
 };
